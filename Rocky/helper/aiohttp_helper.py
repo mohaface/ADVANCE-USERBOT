9d2bd32348597f11ff8b7daa9974ca0acfecaa8c
moha <175589888+mohaface@users.noreply.github.com>
2024-12-21 22:38:24 +0330

    Update aiohttp_helper.py

--- Rocky/helper/aiohttp_helper.py
+++ Rocky/helper/aiohttp_helper.py
@@ -1,35 +1,41 @@
 import json
 import aiohttp
-from aiohttp import ClientSession
+import ssl
+from aiohttp import ClientSession, TCPConnector
 
 class AioHttp:
     @staticmethod
     async def get_json(link):
-        async with ClientSession(trust_env=True) as session:
+        connector = TCPConnector(ssl=False)
+        async with ClientSession(connector=connector) as session:
             async with session.get(link) as resp:
                 return await resp.json()
 
     @staticmethod
     async def get_text(link):
-        async with ClientSession(trust_env=True) as session:
+        connector = TCPConnector(ssl=False)
+        async with ClientSession(connector=connector) as session:
             async with session.get(link) as resp:
                 return await resp.text()
 
     @staticmethod
     async def get_json_from_text(link):
-        async with ClientSession(trust_env=True) as session:
+        connector = TCPConnector(ssl=False)
+        async with ClientSession(connector=connector) as session:
             async with session.get(link) as resp:
                 text = await resp.text()
                 return json.loads(text)
 
     @staticmethod
     async def get_raw(link):
-        async with ClientSession(trust_env=True) as session:
+        connector = TCPConnector(ssl=False)
+        async with ClientSession(connector=connector) as session:
             async with session.get(link) as resp:
                 return await resp.read()
 
     @staticmethod
     async def get_url(link):
-        async with ClientSession(trust_env=True) as session:
+        connector = TCPConnector(ssl=False)
+        async with ClientSession(connector=connector) as session:
             async with session.get(link) as resp:
                 return resp.url
