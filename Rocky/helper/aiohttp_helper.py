c26e597c7aa644cb1c0317707dce283d804f4b06
moha <175589888+mohaface@users.noreply.github.com>
2024-12-21 22:30:27 +0330

    Update aiohttp_helper.py

--- Rocky/helper/aiohttp_helper.py
+++ Rocky/helper/aiohttp_helper.py
@@ -1,36 +1,35 @@
 import json
-
 import aiohttp
-
+from aiohttp import ClientSession
 
 class AioHttp:
     @staticmethod
     async def get_json(link):
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession(trust_env=True) as session:
             async with session.get(link) as resp:
                 return await resp.json()
 
     @staticmethod
     async def get_text(link):
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession(trust_env=True) as session:
             async with session.get(link) as resp:
                 return await resp.text()
 
     @staticmethod
     async def get_json_from_text(link):
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession(trust_env=True) as session:
             async with session.get(link) as resp:
                 text = await resp.text()
                 return json.loads(text)
 
     @staticmethod
     async def get_raw(link):
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession(trust_env=True) as session:
             async with session.get(link) as resp:
                 return await resp.read()
 
     @staticmethod
     async def get_url(link):
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession(trust_env=True) as session:
             async with session.get(link) as resp:
                 return resp.url
